2f5f6db2e34cd74da37c2a43261c09a3228ea630
Vitor Reche <107084549+SizeK1ng@users.noreply.github.com>
2022-10-19 10:48:39 -0300

    Update Main.java

--- appdownloader/app_de_baixar/src/app_de_baixar/Main.java
+++ appdownloader/app_de_baixar/src/app_de_baixar/Main.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class Main {
 
 	public static void main(String[] args) { // insira o link do pdf que você deseja
-		String link = "https://www.tiktok.com/@flowpdc/video/7156040916967410949?is_from_webapp=1&sender_device=pc";
+		String link = "https://livraria-camara-leg.usrfiles.com/ugd/5ca0e9_77426ca451ec4f60b14af67f925f038e.pdf";
 		File out = new File("C:\\Users\\Vituuuu\\Desktop\\aqui\\Livro.pdf"); // cole o diretorio do e o nome do arquivo.pdf 
 		
 		new Thread(new Download(link, out )).start();
